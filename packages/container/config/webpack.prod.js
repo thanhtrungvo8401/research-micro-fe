5f85b5131ad98cea6826967e3a92e36350dbf1f7
TrungVo <thanhtrungvo8401@gmail.com>
2023-07-16 13:32:13 +0700

    create deploy scrypt

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,1 +1,24 @@
-  
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const proConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, proConfig);
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,1 +1,23 @@
-  
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const proConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': `./src/bootstrap.js`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, proConfig);
